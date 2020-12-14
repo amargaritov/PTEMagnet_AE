fda6bcb2cbaf08673d6b5f3debc4a21981922241
amargari <amargari@node-0.ae-test2.arm-page-walks-pg0.wisc.cloudlab.us>
2020-12-14 01:20:20 -0600

    add launch_all_exps script

---
+++ evaluation/launch_all_exps.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app mcf     --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app mcf     --num_experiments 10 --result_dir $(pwd)/results
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app omnetpp --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app omnetpp --num_experiments 10 --result_dir $(pwd)/results
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app xz      --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app xz      --num_experiments 10 --result_dir $(pwd)/results
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app gcc     --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app gcc     --num_experiments 10 --result_dir $(pwd)/results
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app cc      --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app cc      --num_experiments 10 --result_dir $(pwd)/results
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app nibble  --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app nibble  --num_experiments 10 --result_dir $(pwd)/results
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app bfs     --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app bfs     --num_experiments 10 --result_dir $(pwd)/results
+
+./launch_exp.py --experiment_tag asplos21_ae --kernel clean    --app pr      --num_experiments 10 --result_dir $(pwd)/results
+./launch_exp.py --experiment_tag asplos21_ae --kernel modified --app pr      --num_experiments 10 --result_dir $(pwd)/results
