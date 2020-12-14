29d76bff02572e87dabd7bb8e0689c7b49259a33
amargari <amargari@node-0.mcf-test.arm-page-walks-pg0.wisc.cloudlab.us>
2020-12-14 05:54:36 -0700

    fix calc_impr.py where there is only one apP

--- evaluation/calc_impr.py
+++ evaluation/calc_impr.py
@@ -12,7 +12,9 @@ df['impr w/ PTEMagnet, %'] = df['impr w/ PTEMagnet, %'].round(decimals=1)
 df['clean'] = df['clean'].round(decimals=0)
 df['modified'] = df['modified'].round(decimals=0)
 
-geo_impr = ( gmean((df['impr w/ PTEMagnet, %'] / 100 + 1).squeeze()) - 1 ) * 100
-#print(df.to_markdown()) 
 print(df)
-print('Geomean performance imporvement delivered by PTEMagnet is ' + "{0:.1f}".format(geo_impr) + '%')
+try:
+    geo_impr = ( gmean((df['impr w/ PTEMagnet, %'] / 100 + 1).squeeze()) - 1 ) * 100
+    print('Geomean performance imporvement delivered by PTEMagnet is ' + "{0:.1f}".format(geo_impr) + '%')
+except IndexError:
+    pass
