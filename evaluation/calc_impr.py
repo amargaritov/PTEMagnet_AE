291bf4b8d7943ec5c256e5366c0bb1087351d308
amargari <amargari@node-0.mcf-test.arm-page-walks-pg0.wisc.cloudlab.us>
2020-12-15 20:38:53 -0700

    imporve scripts for displaying results

--- evaluation/calc_impr.py
+++ evaluation/calc_impr.py
@@ -7,7 +7,13 @@ from scipy.stats.mstats import gmean
 df = pd.read_csv(sys.stdin, names=["App", "Kernel", "ExecTime"])
 
 df = df.pivot(index='App', columns='Kernel', values="ExecTime")
-df['impr w/ PTEMagnet, %'] = (1 - df['modified'] / df['clean'] ) * 100
+try:
+    df['impr w/ PTEMagnet, %'] = (1 - df['modified'] / df['clean'] ) * 100
+except KeyError:
+    print "Somthing went wrong. Most likely you don't have any results for the modified version of the kernel,\n\
+            so this script does not know how to compute the difference. Get some results for the modified kerneland rerun me."
+    exit(1)
+
 df['impr w/ PTEMagnet, %'] = df['impr w/ PTEMagnet, %'].round(decimals=1)
 df['clean'] = df['clean'].round(decimals=0)
 df['modified'] = df['modified'].round(decimals=0)
--- evaluation/show_results.sh
+++ evaluation/show_results.sh
@@ -1,1 +1,13 @@
-for i in $(ls ./results); do pushd ./results/$i > /dev/null; avg_time=$(grep -R Test_time | awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }'); echo "$i $avg_time"; popd > /dev/null ; done | sed 's/-on-/,/g' | sed 's/-kernel\ */,/g' | $REPO_ROOT/evaluation/calc_impr.py
+RESULTS_DIR="$1"
+if [ -z "$1" ]
+  then
+    RESULTS_DIR=$REPO_ROOT/evaluation/results
+fi
+
+
+echo "Using results from $RESULTS_DIR"
+if [ ! -d $RESULTS_DIR ]; then 
+   echo "can't find directory with the results. Are you sure it exists?"
+else
+   for i in $(ls $RESULTS_DIR); do pushd $RESULTS_DIR/$i > /dev/null; avg_time=$(grep -R Full_time | awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }'); echo "$i $avg_time"; popd > /dev/null ; done | sed 's/-on-/,/g' | sed 's/-kernel\ */,/g' | $REPO_ROOT/evaluation/calc_impr.py
+  fi
