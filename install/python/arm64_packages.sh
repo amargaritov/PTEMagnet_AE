28026d61b129220e894dd8c7f0d49c9a6892e23a
Artemiy Margaritov <amargaritov@srv10.huaweirc.ch>
2020-12-25 03:33:16 +0000

    add list packages required for python on the arm64 server

---
+++ install/python/arm64_packages.sh
@@ -0,0 +1,7 @@
+  sudo apt update 
+  sudo apt install python2 
+  sudo apt install python2-pip
+  sudo apt install python3-pip 
+  sudo apt install python3-virtualenv
+  sudo apt-get install python-dev 
+  sudo apt-get install gfortran libopenblas-dev liblapack-dev
